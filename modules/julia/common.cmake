8d1379b689180f06e26e16a90ff5642723619a58
clynamen <clynamen@gmail.com>
2019-05-26 09:42:28 +0200

    add julia module

--- modules/julia/common.cmake
+++ modules/julia/common.cmake
@@ -1,11 +1,33 @@
 # This file is included from a subdirectory
 set(PYTHON_SOURCE_DIR "${CMAKE_CURRENT_LIST_DIR}")
 
-ocv_add_module(${MODULE_NAME} BINDINGS PRIVATE_REQUIRED opencv_python_bindings_generator)
+ocv_add_module(${MODULE_NAME} BINDINGS PRIVATE_REQUIRED opencv_julia_bindings_generator)
+
 
 include_directories(SYSTEM
-    "${${PYTHON}_INCLUDE_PATH}"
-    ${${PYTHON}_NUMPY_INCLUDE_DIRS}
+    "/home/clynamen/.julia/packages/CxxWrap/sarOk/deps/usr/include/"
+    "/usr/include/julia/"
+    ${CMAKE_SOURCE_DIR}/modules/calib3d/include
+    ${CMAKE_SOURCE_DIR}/modules/core/include
+    ${CMAKE_SOURCE_DIR}/modules/flann/include
+    ${CMAKE_SOURCE_DIR}/modules/dnn/include
+    ${CMAKE_SOURCE_DIR}/modules/features2d/include
+    ${CMAKE_SOURCE_DIR}/modules/flann/include
+    ${CMAKE_SOURCE_DIR}/modules/gapi/include
+    ${CMAKE_SOURCE_DIR}/modules/highgui/include
+    ${CMAKE_SOURCE_DIR}/modules/imgcodecs/include
+    ${CMAKE_SOURCE_DIR}/modules/imgproc/include
+    ${CMAKE_SOURCE_DIR}/modules/ml/include
+    ${CMAKE_SOURCE_DIR}/modules/photo/include
+    ${CMAKE_SOURCE_DIR}/modules/stitching/include
+    ${CMAKE_SOURCE_DIR}/modules/ts/include
+    ${CMAKE_SOURCE_DIR}/modules/video/include
+    ${CMAKE_SOURCE_DIR}/modules/videoio/include
+    ${CMAKE_SOURCE_DIR}/modules/world/include
+    ${CMAKE_SOURCE_DIR}/modules/objdetect/include
+
+    #"${${PYTHON}_INCLUDE_PATH}"
+    #${${PYTHON}_NUMPY_INCLUDE_DIRS}
 )
 ocv_module_include_directories(
     "${PYTHON_SOURCE_DIR}/src2"
@@ -21,9 +43,11 @@ endif()
 
 ocv_add_library(${the_module} MODULE ${PYTHON_SOURCE_DIR}/src2/cv2.cpp ${cv2_generated_hdrs} ${opencv_userdef_hdrs} ${cv2_custom_hdr})
 message("Python module the_module\n\n${the_module}\n\n")
-if(TARGET gen_opencv_python_source)
-  add_dependencies(${the_module} gen_opencv_python_source)
+if(TARGET gen_opencv_julia_source)
+  add_dependencies(${the_module} gen_opencv_julia_source)
 endif()
+
+set_property(TARGET ${the_module} PROPERTY CXX_STANDARD 14)
 
 if(APPLE)
   set_target_properties(${the_module} PROPERTIES LINK_FLAGS "-undefined dynamic_lookup")
@@ -35,9 +59,9 @@ elseif(WIN32 OR OPENCV_FORCE_PYTHON_LIBS)
   endif()
 endif()
 
-if(TARGET gen_opencv_python_source)
+if(TARGET gen_opencv_julia_source)
   set(deps ${OPENCV_MODULE_${the_module}_DEPS})
-  list(REMOVE_ITEM deps opencv_python_bindings_generator) # don't add dummy module to target_link_libraries list
+  list(REMOVE_ITEM deps opencv_julia_bindings_generator) # don't add dummy module to target_link_libraries list
 endif()
 ocv_target_link_libraries(${the_module} LINK_PRIVATE ${deps})
 
@@ -132,7 +156,7 @@ if(NOT " ${PYTHON}" STREQUAL " PYTHON"
 endif()
 
 if(NOT OPENCV_SKIP_PYTHON_LOADER AND DEFINED OPENCV_PYTHON_INSTALL_PATH)
-  include("${CMAKE_CURRENT_LIST_DIR}/python_loader.cmake")
+  include("${CMAKE_CURRENT_LIST_DIR}/julia_loader.cmake")
   set(OPENCV_PYTHON_INSTALL_PATH_SETUPVARS "${OPENCV_PYTHON_INSTALL_PATH}" CACHE INTERNAL "")
 endif()
 
